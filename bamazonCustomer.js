ccd2d8a4c63fc139a8ca977b3b01c7392883941b
Michael D <miketdennison@gmail.com>
2019-07-09 13:19:57 -0500

    part 1 mvp

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,31 @@
+/**
+ * Global Variables & Packages
+ */
 require("dotenv").config();
 var mysql = require("mysql");
-var inquirer = require('inquirer');
-var connection = mysql.createConnection({
+var inquirer = require("inquirer");
+
+/**
+ * Create Connection
+ */
+var options = {
     host: process.env.DB_HOST,
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
     database: "bamazon"
-});
-
+};
+var connection = mysql.createConnection(options);
 connection.connect(function (err) {
     if (err) throw err;
-    printProducts();
+    displayProducts();
 });
 
-function printProducts() {
-    connection.query('SELECT * FROM products', function (err, res) {
+/**
+ * @desc Formats and displays the products in the database then call the main menu
+ */
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log("============================================");
         for (var i = 0; i < res.length; i++) {
@@ -26,12 +36,15 @@ function printProducts() {
             console.log("On Hand Quantity: " + res[i].stock_quantity);
             console.log("============================================");
         }
-        console.log("\n")
-        printMenu();
+        menu();
     });
 }
 
-function printMenu() {
+/**
+ * @desc Use inquirer to get input from the user about items they 'd like to purchase
+ * Afterwards, query the DB based on the user's inputed purchase options
+ */
+function menu() {
     inquirer
         .prompt([{
                 type: "input",
@@ -45,24 +58,53 @@ function printMenu() {
             }
         ])
         .then(function (iResponse) {
-            connection.query("SELECT * FROM products where item_id =" + '"' + iResponse.itemId + '"', function (err, res) {
-                if (err) throw err;
-                if (res.length) {
-                    var itemId = iResponse.itemId;
-                    var onHand = res[0].stock_quantity;
-                    var requestedAmt = iResponse.purchaseAmt;
-
-                    if (onHand < requestedAmt) console.log("Insufficient Stock.");
-                    else processOrder(itemId, requestedAmt);
-                } else {
-                    console.log("Invalid item ID.");
-                }
-                connection.end();
-            });
+            queryDB(iResponse);
         });
 }
 
-function processOrder(itemId, requestedAmt) {
-    console.log(itemId, requestedAmt);
-    console.log("Success!");
+/**
+ * @desc Query the database based on the users inputed purchase options, then process the order
+ * if the order number is valid and there is enough product for their purchase
+ * Process their order and finally, end the connection to host
+ * @param iResponse is passed from the anonymous function defined by inquirer in the menu function
+ */
+function queryDB(iResponse) {
+    connection.query("SELECT * FROM products WHERE item_id = ?",
+        [
+            iResponse.itemId,
+        ],
+        function (err, res) {
+            if (err) throw err;
+            if (res.length) {
+                var itemId = iResponse.itemId;
+                var onHand = res[0].stock_quantity;
+                var requestedAmt = iResponse.purchaseAmt;
+                var price = res[0].price;
+                if (onHand < requestedAmt) console.log("Insufficient Stock.");
+                else processOrder(itemId, requestedAmt, onHand, price);
+            } else {
+                console.log("Invalid item ID.");
+            }
+            connection.end();
+        });
+}
+
+/**
+ * @desc Update the database based on the users inputed purchase options
+ * then display the price
+ * @param itemId the unique item id the user requested to purchase, defined in queryDB function
+ * @param requestedAmt the quantity of the item the user requested to purchase, defined in queryDB function
+ * @param onHand the on hand quantity of the item the user requested to purchase, defined in queryDB function
+ * @param price the price of the item the user requested to purchase, defined in queryDB function
+ */
+function processOrder(itemId, requestedAmt, onHand, price) {
+    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+    [
+        onHand-requestedAmt,
+        itemId,
+    ],
+    function (err) {
+        if(err) throw err;
+        console.log(`Your Total: \$${(price * requestedAmt).toFixed(2)}`);
+    });
 }
