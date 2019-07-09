738d1a9d8852dcb8595e679b05b31c3e622d32ab
Michael D <miketdennison@gmail.com>
2019-07-09 08:01:54 -0500

    added menu and retrieval of data

--- bamazon.sql
+++ bamazon.sql
@@ -9,3 +9,27 @@ CREATE TABLE IF NOT EXISTS products (
     price float(10,2),
     stock_quantity int
 );
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Thing", "Things", 100.00, 20);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Stuff", "Stuff", 4.99, 0);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Other", "Misc.", 20.00, 10);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Battle Necks", "Candy", 0.30, 100);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Dummies", "Candy", 0.10, 1000);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Fingies Squares", "Candy", 0.50, 9);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Cheese-Thing", "Snack", 2.99, 1);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Tire", "Auto", 150.00, 4);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Big Crunch", "Gum", .99, 3);
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Creed", "Bad Music", 0.00, 0);
+
+USE bamazon;
+select * from products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,68 @@
 require("dotenv").config();
 var mysql = require("mysql");
-
+var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: 3306,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PW,
     database: "bamazon"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
+    printProducts();
 });
 
-function afterConnection() {
-    /*
-    connection.query("SELECT * FROM products", function (err, res) {
+function printProducts() {
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        console.log("============================================");
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID         : " + res[i].item_id);
+            console.log("Product Name    : " + res[i].product_name);
+            console.log("Department      : " + res[i].department_name);
+            console.log("Price           : " + res[i].price.toFixed(2));
+            console.log("On Hand Quantity: " + res[i].stock_quantity);
+            console.log("============================================");
+        }
+        console.log("\n")
+        printMenu();
     });
-    */
+}
+
+function printMenu() {
+    inquirer
+        .prompt([{
+                type: "input",
+                message: "What is the item ID of the product you'd like to buy?",
+                name: "itemId"
+            },
+            {
+                type: "input",
+                message: "How many would you like to purchase?",
+                name: "purchaseAmt"
+            }
+        ])
+        .then(function (iResponse) {
+            connection.query("SELECT * FROM products where item_id =" + '"' + iResponse.itemId + '"', function (err, res) {
+                if (err) throw err;
+                if (res.length) {
+                    var itemId = iResponse.itemId;
+                    var onHand = res[0].stock_quantity;
+                    var requestedAmt = iResponse.purchaseAmt;
+
+                    if (onHand < requestedAmt) console.log("Insufficient Stock.");
+                    else processOrder(itemId, requestedAmt);
+                } else {
+                    console.log("Invalid item ID.");
+                }
+                connection.end();
+            });
+        });
+}
+
+function processOrder(itemId, requestedAmt) {
+    console.log(itemId, requestedAmt);
+    console.log("Success!");
 }
